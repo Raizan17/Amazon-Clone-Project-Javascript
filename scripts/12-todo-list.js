e870f135df04852a5db183ac461111041fc5ab50
Raizan17 <raizan.lee17@gmail.com>
2023-06-26 19:08:23 -0600

    Reupload of entire code base
    Reuploaded the entire code base, using Github Desktop, so that files are more organized within folders.

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,66 @@
+const todoList = [{
+  name:'make dinner',
+  dueDate: '2022-12-22'
+},{
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+  } ];
+
+renderTodoList();
+
+function renderTodoList() {
+
+  let todoListHTML = '';
+
+  todoList.forEach(function(todoObject, index){
+
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = 
+    `<div>${name}</div>
+    <div>${dueDate}</div>
+    <button class="delete-todo-button">Delete</button>
+    `;
+
+    todoListHTML= todoListHTML + html
+    
+
+  })
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button').forEach(
+    (deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+    todoList.splice(index, 1);
+    renderTodoList();
+      });
+    }
+  );
+
+  }
+
+function addTodo() {
+const inputElement = document.querySelector('.js-name-input');
+const name = inputElement.value;
+
+const dateInputElement = document.querySelector('.js-due-date.input');
+
+const dueDate = dateInputElement.value;
+
+todoList.push({
+ // name: name,
+ // dueDate: dueDate,
+  name,
+  dueDate
+
+});
+console.log(todoList);
+
+inputElement.value = '';
+
+renderTodoList();
+
+}
